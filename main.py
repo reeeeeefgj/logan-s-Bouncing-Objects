97b1a4116dd05d09392dd31a8df2d6aff9d94ebb
reeeeeefgj <logan.schmer@gmail.com>
2026-04-20 15:38:59 -0500

    idk just finished it

--- main.py
+++ main.py
@@ -9,82 +9,142 @@ turtle = Turtle()
 turtle.color("white")
 turtle.shape("circle")
 turtle.up()
+
 pen = Turtle()
 pen.hideturtle()
 pen.color("white")
-pen.up()
-pen.goto(-300,300)
-pen.down()
-pen.goto(-300,-300)
-pen.goto(300,-300)
-pen.goto(300,300)
-pen.goto(-300,300)
-pen.up()
-pen.home()
 
-#                 x y
-poition = { 0: [0,0]}
-directions = [random.randint(0,360)]
+player = Turtle()
+player.color("red")
+player.shape("circle")
+player.shapesize(1.5)
+player.up()
+player.hideturtle()
 
+# List of turtle objects: each {'pos': [x,y], 'dir': angle, 'active': bool}
+turtles = [{'pos': [0, 0], 'dir': random.randint(0, 360), 'active': True}]
 
-
-amount_turtle = 1
 speed = 5
-direction = random.randint(0,360)
-def bounce(turtle_poition,direction):
-    global amount_turtle, poition, directions
-    
-    if turtle_poition[0] > 290:
-        direction = 180 - direction
-        amount_turtle += 1
-        poition[amount_turtle - 1] = [turtle_poition[0] - 5, turtle_poition[1]]
-        directions.append(random.randint(91,179))
-    elif turtle_poition[0] < -290:
-        direction = 0 + (180 - direction)
-        amount_turtle += 1
-        poition[amount_turtle - 1] = [turtle_poition[0] + 5, turtle_poition[1]]
-        directions.append(random.randint(91,179) + 180)
-    elif turtle_poition[1] > 290:
-        direction = 270 + (90 - direction)
-        amount_turtle += 1
-        poition[amount_turtle - 1] = [turtle_poition[0], turtle_poition[1] - 5]
-        directions.append(random.randint(181,359))
-    elif turtle_poition[1] < -290:
-        direction = 90 + (270 - direction)
-        amount_turtle += 1
-        poition[amount_turtle - 1] = [turtle_poition[0], turtle_poition[1] + 5]
-        directions.append(random.randint(1,179))
 
-    return direction
+key_pressed = { "a" : False, "d" : False, "space" : False}
 
 
-def turtle_run(turtle_index):
-    
-    global directions, poition, turtle
-    
-    turtle_poition = poition.get(turtle_index)
 
-    turtle.goto(turtle_poition[0], turtle_poition[1])
+
+
+def bounce(turtle_pos, direction, pen, turtles):
     
-    if turtle_poition[0] >= 290 or turtle_poition[0] <= -290 or turtle_poition[1] >= 290 or turtle_poition[1] <= -290:
-        directions[turtle_index] = bounce(turtle_poition, directions[turtle_index])
-    turtle.setheading(directions[turtle_index])
+    new_dir = direction
+    if turtle_pos[0] > 290:
+        new_dir = 180 - direction
+        if pen == "turtle":
+            turtles.append({'pos': [turtle_pos[0] - 5, turtle_pos[1]], 'dir': random.randint(91, 179), 'active': True})
+    elif turtle_pos[0] < -290:
+        new_dir = 0 + (180 - direction)
+        if pen == "turtle":
+            turtles.append({'pos': [turtle_pos[0] + 5, turtle_pos[1]], 'dir': random.randint(91, 179) + 180, 'active': True})
+    elif turtle_pos[1] > 290:
+        new_dir = 270 + (90 - direction)
+        if pen == "turtle":
+            turtles.append({'pos': [turtle_pos[0], turtle_pos[1] - 5], 'dir': random.randint(181, 359), 'active': True})
+    elif turtle_pos[1] < -290:
+        new_dir = 90 + (270 - direction)
+        if pen == "turtle":
+            turtles.append({'pos': [turtle_pos[0], turtle_pos[1] + 5], 'dir': random.randint(1, 179), 'active': True})
+
+    return new_dir
+
+
+def turtle_run(turtle_obj):
+    if not turtle_obj['active']:
+        return
+    
+    if key_pressed.get("space") and player.distance(turtle_obj['pos'][0], turtle_obj['pos'][1]) < 20:
+        turtle_obj['active'] = False
+        return
+    
+    turtle_pos = turtle_obj['pos']
+
+    turtle.goto(turtle_pos[0], turtle_pos[1])
+    
+    if turtle_pos[0] >= 290 or turtle_pos[0] <= -290 or turtle_pos[1] >= 290 or turtle_pos[1] <= -290:
+        turtle_obj['dir'] = bounce(turtle_pos, turtle_obj['dir'], "turtle", turtles)
+    turtle.setheading(turtle_obj['dir'])
     turtle.forward(speed)
         
-    poition[turtle_index] = [turtle.xcor(), turtle.ycor()]
+    turtle_obj['pos'] = [turtle.xcor(), turtle.ycor()]
+    
     turtle.stamp()
     
-        
+def key_down(key):
+    global keys_pressed
+    key_pressed[key] = True
 
-direction = random.randint(0,360)
+def key_up(key):
+    global keys_pressed
+    key_pressed[key] = False
+
+def player_movement():
+    global player
+
+    turtle_pos = [player.xcor(), player.ycor()]
+    if turtle_pos[0] >= 290 or turtle_pos[0] <= -290 or turtle_pos[1] >= 290 or turtle_pos[1] <= -290:
+        player.setheading(bounce(turtle_pos, player.heading(), "player", turtles))
+
+    for key, on in key_pressed.items():
+        
+        if on:
+            
+            
+            if key == "a":
+                player.left(2.5)
+            elif key == "d":
+                player.right(2.5)
+                
+    player.forward(speed)
+    player.stamp()
+    
+
+
 
 def main():
 
+
+    pen.up()
+    pen.goto(-300,300)
+    pen.down()
+    pen.goto(-300,-300)
+    pen.goto(300,-300)
+    pen.goto(300,300)
+    pen.goto(-300,300)
+    pen.up()
+    pen.home()
+
+
     window.tracer(0)
     while True:
-        for i in range(amount_turtle):
-            turtle_run(i)
+        for turtle_obj in turtles:
+            turtle_run(turtle_obj)
+        if key_pressed.get("space"):
+            player_movement()
         window.update()
         turtle.clear()
+        player.clear()
+
+
+
+
+
+window.listen()
+
+
+window.onkeypress(lambda: key_down("space"), "space")
+
+window.onkeypress(lambda: key_down("a"), "a")
+window.onkeyrelease(lambda: key_up("a"), "a")
+window.onkeypress(lambda: key_down("d"), "d")
+window.onkeyrelease(lambda: key_up("d"), "d")
+
+
 main()
-mainloop()
+window.exitonclick()
